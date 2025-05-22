25e5d522725b646a1e4f5f9c5850dcf1b46180c2
NoUser <chuah_cheng_hang@s2024.ssts.edu.sg>
2025-05-22 21:04:35 +0800

    commit

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -8,16 +8,24 @@ export default function Index() {
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>DHANVIN</Text>
             <Text style={styles.textTitle}>VOTES</Text>
+            <View style={styles.horizontalBoxContainer}>
+              <View style={styles.box}>
+                <Text style={styles.votesTitle}>Yes</Text>
+              </View>
+              <View style={styles.box}>
+                <Text style={styles.votesTitle}>No</Text>
+              </View>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 24,
@@ -28,5 +36,23 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
     fontWeight: 'bold',
+  },
+  horizontalBoxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 160,
+    height: 200,
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  votesTitle: {
+    fontSize: 50, 
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
   }
 })
